560fb23b228bc671a3bae428ca0ca1a756589cdc
Miranda Ibarra Melina <mirandaibarramelina@gmail.com>
2025-08-28 19:17:05 -0300

    Creacion de carpeta Persistence en Infrastructure y creacion de AppDbContext

--- MenuRestaurante/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ MenuRestaurante/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60835642aa4fa4c00b321cb8bf1138d8b5981a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c59b9eac5110929622e955f8e05a35799074269")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
